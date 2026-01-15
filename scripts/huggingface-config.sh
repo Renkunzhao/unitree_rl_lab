8fb0fea1f40399ee5ab135df03eef0ac3c71da8e
Kunzhao Ren <rkzdtc@gmail.com>
2026-01-14 22:15:47 -0600

    huggingface setup

---
+++ scripts/huggingface-config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 获取项目根目录
+PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/../
+echo "Project Directory: $PROJECT_DIR"
+cd $PROJECT_DIR
+
+apt install python3.10-venv
+curl -LsSf https://hf.co/cli/install.sh | bash
+hf auth login
+hf auth whoami
+# hf upload legged-ai-lab/unitree_rl_lab logs/ 
+
+hf download legged-ai-lab/unitree_rl_lab --local-dir logs/
