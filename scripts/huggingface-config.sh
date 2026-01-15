eb857f5b0416fa65020b79522a2f6bb547995251
Kunzhao Ren <rkzdtc@gmail.com>
2026-01-16 02:15:52 +0800

    aarch64 compatible

--- scripts/huggingface-config.sh
+++ scripts/huggingface-config.sh
@@ -5,8 +5,11 @@ PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/../
 echo "Project Directory: $PROJECT_DIR"
 cd $PROJECT_DIR
 
-apt install python3.10-venv
+apt install -y python3.10-venv
 curl -LsSf https://hf.co/cli/install.sh | bash
+echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+
 hf auth login
 hf auth whoami
 # hf upload legged-ai-lab/unitree_rl_lab logs/ 
